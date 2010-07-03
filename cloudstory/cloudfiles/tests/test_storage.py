6d5effdc73f983e8d69a2c1a1deb6d315e350268
brianjinwright <brianjinwright@brianjinwright>
2010-07-03 12:42:09 -0400

    Updated CloudFiles Tests and Test Settings

--- cloudstory/cloudfiles/tests/test_storage.py
+++ cloudstory/cloudfiles/tests/test_storage.py
@@ -7,7 +7,8 @@ import unittest
 from test_settings import\
 CLOUDFILES_AUTH_KEY,CLOUDFILES_AUTH_USER,\
 CLOUDFILES_CONTAINER,CLOUDFILES_NEW_CONTAINER,\
-CLOUDFILES_B_AUTH_USER,CLOUDFILES_B_AUTH_KEY 
+CLOUDFILES_B_AUTH_USER,CLOUDFILES_B_AUTH_KEY,\
+CLOUDFILES_DELETE_OBJECT,CLOUDFILES_DELETE_OBJECT_B 
 
 
 from cloudstory.cloudfiles import CloudFiles
@@ -120,7 +121,42 @@ class CloudFilesAuthTestCase(unittest.TestCase):
             )
         self.assertEquals(action.response.get('status'),'204')
         
-
+    def testXDeleteObject(self):
+        action = self.cf(
+            'delete_object',
+            auth_token=self.cf.auth_token,
+            request_url=self.cf.storage_url,
+            request_path='%s/%s' % (CLOUDFILES_CONTAINER,CLOUDFILES_DELETE_OBJECT)
+            )
+        self.assertEquals(action.response.get('status'),'204')
+        
+    def testObjectMetaHeaders(self):
+        action = self.cf(
+            'object_metadata_headers',
+            auth_token=self.cf.auth_token,
+            request_url=self.cf.storage_url,
+            request_path='%s/%s' % (CLOUDFILES_CONTAINER,CLOUDFILES_DELETE_OBJECT)
+            )
+        self.assertEquals(action.response.get('status'),'204')
+        self.failIf(
+            not action.response.has_key(
+                'content-type'),
+            'Response doesn not have content-type')
+        actionb = self.cf(
+            'object_metadata_headers',
+            auth_token=self.cf.auth_token,
+            request_url=self.cf.storage_url,
+            request_path='%s/%s' % (CLOUDFILES_CONTAINER,CLOUDFILES_DELETE_OBJECT_B)
+            )
+        self.assertEquals(actionb.response.get('status'),'204')
+        self.failIf(
+            not actionb.response.has_key(
+                'content-type'),
+            'Response doesn not have content-type')
+        self.failIf(
+            action.response.get('etag') == actionb.response.get('etag'),
+            'It is looking at the same file check your delete object settings.')
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
