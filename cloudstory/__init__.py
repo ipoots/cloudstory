5199d6e0abe6d594fa22034b5b1aa8b5ca052db5
brianjinwright <brianjinwright@brianjinwright>
2010-07-03 11:03:17 -0400

    Updated Drivers

--- cloudstory/__init__.py
+++ cloudstory/__init__.py
@@ -1,11 +1,17 @@
-from cloudstory.nirvanix import Nirvanix
+from nirvanix import Nirvanix
+from cloudfiles import CloudFiles
 
-DRIVERS = {'nirvanix':Nirvanix}
+DRIVERS = {
+    'nirvanix':Nirvanix,
+    'cloudfiles':CloudFiles
+    }
+
 class DriverException(Exception):
     def __init__(self,message):
         self.message = message
     def __str__(self):
         return repr("CloudStory does not have a driver for %s" % (self.message)) 
+
 def get_driver(driver):
     if DRIVERS.has_key(str(driver).lower()):
         return DRIVERS.get(str(driver).lower())
